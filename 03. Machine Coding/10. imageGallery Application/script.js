815a50b251888d48be47a1a6316f58c7e6bc06a4
Nadeem <nadeemasgar18@gmail.com>
2024-01-07 01:25:58 +0530

    event delegation used

--- 03. Machine Coding/10. imageGallery Application/script.js
+++ 03. Machine Coding/10. imageGallery Application/script.js
@@ -1,3 +1,4 @@
+/* 
 let images = document.querySelectorAll(".image-conatiner img");
 
 for (let image of images) {
@@ -12,3 +13,20 @@ for (let image of images) {
     viewImage.style.display = "none";
   });
 }
+ */
+
+let imageContainer = document.querySelector(".image-conatiner");
+let fullImageConatiner = document.querySelector(".view-image");
+let closeBtn = document.querySelector(".view-image span");
+
+imageContainer.addEventListener("click", function (event) {
+  if (event.target.localName === "img") {
+    fullImageConatiner.style.display = "flex";
+    let fullImage = fullImageConatiner.querySelector("img");
+    fullImage.src = event.target.src;
+  }
+});
+
+closeBtn.addEventListener("click", function (event) {
+  fullImageConatiner.style.display = "none";
+});
